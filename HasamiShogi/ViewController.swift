18ed74843ec892badca4ea339d0dfbf5886c7b2c
kiko <kiko@d67.private>
2015-02-09 15:15:29 +0900

    Initial Commit

---
+++ HasamiShogi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HasamiShogi
+//
+//  Created by kiko on 2015/02/09.
+//  Copyright (c) 2015年 nemupm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
